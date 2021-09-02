cc8eb844df00cd69e0e5135e544093073a2d385d
@ArtemMikhalev <mikhalevardm@gmail.com>
2021-09-02 04:58:35 +0300

    add StyledButton ui component

--- src/components/JokeList.vue
+++ src/components/JokeList.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="joke" v-for="joke in jokes">
     <div>{{ joke.joke }}</div>
+    <div class="joke__buttons">
+      <styled-button>Like!</styled-button>
+    </div>
   </div>
 </template>
 
 <script>
+import StyledButton from "./UI/StyledButton.vue";
 export default {
+  components: { StyledButton },
   props: {
     jokes: {
       type: Array,
@@ -19,8 +24,10 @@ export default {
 .joke {
   display: flex;
   justify-content: space-between;
+  align-items: center;
   padding: 20px;
   border: 2px solid #5562f5;
   margin-top: 10px;
+  border-radius: 5px;
 }
 </style>
---
+++ src/components/UI/StyledButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="button"><slot></slot></button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.button {
+  background: none;
+  padding: 5px 5px;
+  border: 2px solid #5562f5;
+  border-radius: 5px;
+  color: #5562f5;
+}
+</style>
